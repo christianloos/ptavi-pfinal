02791dce09c9bb121391a2d3ee5c8e7aa2a442be
Cristian  Loos Beneit <cloos@alpha14.aulas.gsyc.urjc.es>
2017-01-10 13:21:09 +0100

    UA Server pasado por PEP8

--- uaserver.py
+++ uaserver.py
@@ -8,10 +8,12 @@ import socketserver
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from uaclient import XmlHandler
-from uaclient import log_reg
 import time
 
-""" Clase manejadora de SIP y RTP en el servidor """
+"""
+Clase manejadora de SIP y RTP en el servidor
+"""
+
 
 class SIP_ServerHandler(socketserver.DatagramRequestHandler):
 
@@ -19,15 +21,15 @@ class SIP_ServerHandler(socketserver.DatagramRequestHandler):
     dest_port = []
 
     def handle(self):
-        
+
         # Leyendo línea a línea lo que envía el cliente
         line = self.rfile.read()
         data = line.decode('utf-8')
-        
+
         chops = data.split()
         REQUEST = chops[0]
-        
-        sip_str = ' SIP/2.0\r\n' 
+
+        sip_str = ' SIP/2.0\r\n'
         trying_str = 'SIP/2.0 100 Trying\r\n\r\n'
         ring_str = 'SIP/2.0 180 Ring\r\n\r\n'
         ok_str = 'SIP/2.0 200 OK\r\n'
@@ -39,13 +41,13 @@ class SIP_ServerHandler(socketserver.DatagramRequestHandler):
             self.dest_ip.append(chops[7])
             self.dest_port.append(chops[11])
             print("Received: " + data)
-            
+
             # Apertura del log
             log_info = 'Received from ' + proxy_ip + ':' + \
-                        str(proxy_port) + ': ' + \
-                        data.replace('\r\n', ' ')
+                       str(proxy_port) + ': ' + \
+                       data.replace('\r\n', ' ')
             log_reg(config_info, log_info)
-            
+
             # Respuesta al cliente
             header = 'Content-Type: application/sdp\r\n\r\n'
             v = 'v=0\r\n'
@@ -57,7 +59,7 @@ class SIP_ServerHandler(socketserver.DatagramRequestHandler):
             sent_line = trying_str + ring_str + ok_str + header + body
             self.wfile.write(bytes(sent_line, 'utf-8') + b'\r\n')
             print("Sending..." + '\r\n' + sent_line)
-            
+
             log_info = 'Sent to ' + proxy_ip + ':' + str(proxy_port) + \
                        ': ' + sent_line.replace('\r\n', ' ')
             log_reg(config_info, log_info)
@@ -65,20 +67,20 @@ class SIP_ServerHandler(socketserver.DatagramRequestHandler):
         # Petición ACK/ Envío RTP
         elif REQUEST == 'ACK':
 
-           # Envío RTP
+            # Envío RTP
             print("Received: " + data)
-            
+
             log_info = 'Received from ' + proxy_ip + ':' + \
-                        str(proxy_port) + ': ' + \
-                        data.replace('\r\n', ' ')
+                       str(proxy_port) + ': ' + \
+                       data.replace('\r\n', ' ')
             log_reg(config_info, log_info)
-            
+
             aEjecutar = 'mp32rtp -i ' + server_ip + ' -p ' + \
-            str(rtp_port) + ' < ' + str(audio)
+                        str(rtp_port) + ' < ' + str(audio)
             print("Executing... ", aEjecutar)
             os.system(aEjecutar)
             print("Successfully sent")
-            
+
             log_info = 'Sent to ' + server_ip + ':' + str(rtp_port) + \
                        ': ' + 'RTP MEDIA'
             log_reg(config_info, log_info)
@@ -86,31 +88,32 @@ class SIP_ServerHandler(socketserver.DatagramRequestHandler):
         elif REQUEST == 'BYE':
 
             print("Received: " + data)
-            
+
             log_info = 'Received from ' + proxy_ip + ':' + \
-                        str(proxy_port) + ': ' + \
-                        data.replace('\r\n', ' ')
+                       str(proxy_port) + ': ' + \
+                       data.replace('\r\n', ' ')
             log_reg(config_info, log_info)
-            
+
             self.wfile.write(bytes(ok_str, 'utf-8') + b'\r\n')
             print("Sending..." + '\r\n' + ok_str)
-            
+
             log_info = 'Sent to ' + proxy_ip + ':' + str(proxy_port) + \
                        ': ' + ok_str.replace('\r\n', ' ')
             log_reg(config_info, log_info)
 
         elif REQUEST not in ['INVITE', 'ACK', 'BYE']:
             self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
-            
+
             log_info = 'Error: SIP/2.0 405 Method Not Allowed.'
             log_reg(config_info, log_info)
 
         else:
             self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
-            
+
             log_info = 'Error: SIP/2.0 400 Bad Request.'
             log_reg(config_info, log_info)
-            
+
+
 # Función de registro de operaciones en un log
 def log_reg(config_info, info):
 
@@ -127,7 +130,7 @@ if __name__ == "__main__":
         XML = str(sys.argv[1])
     except:
         sys.exit("Usage: python3 uaserver.py config")
-        
+
     # Instanciación del manejador del archivo de configuración
     parser = make_parser()
     cHandler = XmlHandler()
@@ -150,12 +153,12 @@ if __name__ == "__main__":
     # Creación del socket con la IP y puerto al que conectarse
     serv = socketserver.UDPServer((server_ip,
                                    server_port),
-                                   SIP_ServerHandler)
+                                  SIP_ServerHandler)
     print("Listening..." + '\r\n')
-    
+
     log_info = 'Starting...'
     log_reg(config_info, log_info)
-    
+
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
